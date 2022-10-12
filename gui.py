800901744524a7703c9ef5cfe33e67650c5f27b4
Francisco Hernández <113263831+FranciscoHT00@users.noreply.github.com>
2022-10-12 13:58:13 -0500

    Initial commit

---
+++ gui.py
@@ -0,0 +1,213 @@
+import sys
+from PyQt6 import QtWidgets as qtw
+from PyQt6 import QtCore as qtc
+from PyQt6 import QtGui as qtg
+import time
+
+from model import AppAutomata
+
+
+class ControlPanel(qtw.QWidget):
+
+    start_signal = qtc.pyqtSignal(str)
+    speed_signal = qtc.pyqtSignal(str)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setLayout(qtw.QVBoxLayout())
+        self.setFixedSize(150, 500)
+
+        self.word_label = qtw.QLabel('PALABRA')
+        self.word_label.setFixedHeight(20)
+        self.word_label.setAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
+        self.word_input = qtw.QLineEdit()
+        self.state_label = qtw.QLabel('ESTADO')
+        self.state_label.setFixedHeight(20)
+        self.state_label.setAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
+        self.state_output = qtw.QTextEdit()
+        self.state_output.setReadOnly(True)
+
+        self.start_button = qtw.QPushButton('Iniciar')
+        self.start_button.clicked.connect(self.start_button_pressed)
+        # self.cancel_button = qtw.QPushButton('Cancelar')
+
+        self.speed_label = qtw.QLabel('VELOCIDAD')
+        self.speed_label.setFixedHeight(20)
+        self.speed_label.setAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
+        self.speed_state = qtw.QLineEdit()
+        self.speed_state.setReadOnly(True)
+
+        self.speed_buttonGroup = qtw.QButtonGroup()
+        # self.step_speed_button = qtw.QPushButton('Paso a paso')
+        # self.step_speed_button.clicked.connect(self.step_speed_button_pressed)
+        self.slow_speed_button = qtw.QPushButton('Lenta')
+        self.slow_speed_button.clicked.connect(self.slow_speed_button_pressed)
+        self.normal_speed_button = qtw.QPushButton('Normal')
+        self.normal_speed_button.clicked.connect(self.normal_speed_button_pressed)
+        self.fast_speed_button = qtw.QPushButton('Rápida')
+        self.fast_speed_button.clicked.connect(self.fast_speed_button_pressed)
+        # self.instant_speed_button = qtw.QPushButton('Instantánea')
+        # self.instant_speed_button.clicked.connect(self.instant_speed_button_pressed)
+        # self.speed_buttonGroup.addButton(self.step_speed_button)
+        self.speed_buttonGroup.addButton(self.slow_speed_button)
+        self.speed_buttonGroup.addButton(self.normal_speed_button)
+        self.speed_buttonGroup.addButton(self.fast_speed_button)
+        # self.speed_buttonGroup.addButton(self.instant_speed_button)
+        # self.next_button = qtw.QPushButton('Siguiente')
+
+        self.layout().addWidget(self.word_label)
+        self.layout().addWidget(self.word_input)
+        self.layout().addWidget(self.state_label)
+        self.layout().addWidget(self.state_output)
+        self.layout().addWidget(self.start_button)
+        # self.layout().addWidget(self.cancel_button)
+        self.layout().addWidget(self.speed_label)
+        self.layout().addWidget(self.speed_state)
+        # self.layout().addWidget(self.step_speed_button)
+        self.layout().addWidget(self.slow_speed_button)
+        self.layout().addWidget(self.normal_speed_button)
+        self.layout().addWidget(self.fast_speed_button)
+        # self.layout().addWidget(self.instant_speed_button)
+        # self.layout().addWidget(self.next_button)
+
+    def start_button_pressed(self):
+        self.start_signal.emit(self.word_input.text())
+
+    def cancel_button_pressed(self):
+        pass
+
+    def step_speed_button_pressed(self):
+        self.speed_signal.emit('step')
+
+    def slow_speed_button_pressed(self):
+        self.speed_signal.emit('slow')
+
+    def normal_speed_button_pressed(self):
+        self.speed_signal.emit('normal')
+
+    def fast_speed_button_pressed(self):
+        self.speed_signal.emit('fast')
+
+    def instant_speed_button_pressed(self):
+        self.speed_signal.emit('instant')
+
+    def next_button_pressed(self):
+        pass
+
+
+class MainWindow(qtw.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('Proyecto Autómata')
+        self.setFixedWidth(1000)
+        self.setFixedHeight(600)
+
+        self.model = AppAutomata()
+
+        self.pixmap = qtg.QPixmap()
+        self.pixmap.load('./img/graph.svg')
+        self.image_label = qtw.QLabel()
+        self.image_label.setPixmap(self.pixmap)
+        self.setCentralWidget(self.image_label)
+
+        self.graph_automata()
+
+        menu_bar = self.menuBar()
+
+        self.automata_menu = menu_bar.addMenu('Automata')
+        self.automata_menu.addAction('Automata a la 5', self.load_automata_a_la_5)
+        self.automata_menu.addAction('Automata - Clausura de Kleene', self.load_automata_kleene)
+
+        language_menu = menu_bar.addMenu('Idioma')
+        language_menu.addAction(qtg.QIcon('./img/en_icon'), 'English')
+        language_menu.addAction(qtg.QIcon('./img/es_icon'), 'Español')
+
+        self.controlPanel_dock = qtw.QDockWidget('Panel de Control')
+        self.controlPanel = ControlPanel()
+        self.controlPanel_dock.setWidget(self.controlPanel)
+        self.controlPanel_dock.setFeatures(qtw.QDockWidget.DockWidgetFeature.NoDockWidgetFeatures)
+
+        self.controlPanel_dock.setAllowedAreas(
+            qtc.Qt.DockWidgetArea.RightDockWidgetArea | qtc.Qt.DockWidgetArea.LeftDockWidgetArea)
+
+        self.addDockWidget(qtc.Qt.DockWidgetArea.RightDockWidgetArea, self.controlPanel_dock)
+
+        self.controlPanel.speed_state.setText(self.model.speed)
+        self.controlPanel.start_signal.connect(self.validate_word)
+        self.controlPanel.speed_signal.connect(self.set_speed)
+
+        self.statusBar().showMessage('Bienvenido a nuestra app!', 4000)
+
+        self.show()
+
+    def load_graph_img(self):
+        self.pixmap.load('./img/graph.svg')
+        self.image_label.setPixmap(self.pixmap)
+        self.update()
+        self.image_label.repaint()
+        qtw.QApplication.processEvents()
+
+    def graph_automata(self):
+
+        self.model.reset_graph()
+        self.load_graph_img()
+
+
+    def load_automata_kleene(self):
+
+        self.model.active_automata = self.model.automata_kleene
+        self.model.active_graph = self.model.graph_kleene
+        self.model.active_automata_tag = 'automata_kleene'
+        self.graph_automata()
+
+    def load_automata_a_la_5(self):
+
+        self.model.active_automata = self.model.automata_a_la_5
+        self.model.active_graph = self.model.graph_a_la_5
+        self.model.active_automata_tag = 'automata_a_la_5'
+        self.graph_automata()
+
+    def validate_word(self, word):
+
+        last = 'q0'
+        self.controlPanel.state_output.clear()
+        try:
+            result = self.model.validate_word(word)
+            for i in result:
+                last = i
+                self.controlPanel.state_output.append(last)
+                # self.model.reset_graph()
+                # self.load_graph_img()
+                # time.sleep(0.5)
+                self.model.update_graph(last)
+                self.load_graph_img()
+
+                if self.model.speed == 'step':
+                    time.sleep(2)
+                elif self.model.speed == 'slow':
+                    time.sleep(2)
+                elif self.model.speed == 'normal':
+                    time.sleep(1)
+                elif self.model.speed == 'fast':
+                    time.sleep(0.5)
+
+        except:
+            self.model.update_graph_error(last)
+            self.load_graph_img()
+            self.controlPanel.state_output.append('La palabra fue rechazada')
+        else:
+            self.controlPanel.state_output.append('La palabra fue aceptada')
+        finally:
+            self.load_graph_img()
+
+    def set_speed(self, speed):
+        self.model.speed = speed
+        self.controlPanel.speed_state.setText(self.model.speed)
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec())
---
+++ model.py
@@ -0,0 +1,138 @@
+from automata.fa.dfa import DFA
+import igraph as ig
+import matplotlib.pyplot as plt
+
+
+class AppAutomata:
+
+    automata_kleene = DFA(
+        states={'q0', 'q1', 'q2', 'q3'},
+        input_symbols={'a', 'b', 'c', 'd'},
+        transitions={
+            'q0': {'a': 'q1', 'b': 'q1', 'c': 'q2', 'd': 'q3'},
+            'q1': {'a': 'q1', 'b': 'q1', 'c': 'q2', 'd': 'q3'},
+            'q2': {'c': 'q2', 'd': 'q3'}
+        },
+        initial_state='q0',
+        final_states={'q3'},
+        allow_partial=True
+    )
+
+    automata_a_la_5 = DFA(
+        states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11'},
+        input_symbols={'a', 'b', 'c', 'd'},
+        transitions={
+            'q0': {'a': 'q1', 'b': 'q1', 'c': 'q6', 'd': 'q11'},
+            'q1': {'a': 'q2', 'b': 'q2', 'c': 'q6', 'd': 'q11'},
+            'q2': {'a': 'q3', 'b': 'q3', 'c': 'q6', 'd': 'q11'},
+            'q3': {'a': 'q4', 'b': 'q4', 'c': 'q6', 'd': 'q11'},
+            'q4': {'a': 'q5', 'b': 'q5', 'c': 'q6', 'd': 'q11'},
+            'q5': {'c': 'q6', 'd': 'q11'},
+            'q6': {'c': 'q7', 'd': 'q11'},
+            'q7': {'c': 'q8', 'd': 'q11'},
+            'q8': {'c': 'q9', 'd': 'q11'},
+            'q9': {'c': 'q10', 'd': 'q11'},
+            'q10': {'d': 'q11'}
+        },
+        initial_state='q0',
+        final_states={'q11'},
+        allow_partial=True
+    )
+
+    graph_kleene = ig.Graph(
+        4,
+        [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2), (1, 3), (2, 2), (2, 3)],
+        directed = True
+    )
+    graph_kleene["title"] = "Automata Clausura de Kleene"
+    graph_kleene.vs["name"] = ["q0", "q1", "q2", "q3"]
+
+    graph_a_la_5 = ig.Graph(
+        12,
+        [(0, 1), (0, 6), (0, 11),
+         (1, 2), (1, 6), (1, 11),
+         (2, 3), (2, 6), (2, 11),
+         (3, 4), (3, 6), (3, 11),
+         (4, 5), (4, 6), (4, 11),
+         (5, 6), (5, 6), (5, 11),
+         (6, 7),(6, 11),
+         (7, 8),(7, 11),
+         (8, 9),(8, 11),
+         (9, 10),(9, 11),
+         (10, 11)],
+        directed = True
+    )
+    graph_a_la_5["title"] = "Automata Clausura de Kleene"
+    graph_a_la_5.vs["name"] = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11"]
+
+    def __init__(self):
+
+        self.language = 'es'
+        self.active_automata = self.automata_kleene
+        self.active_graph = self.graph_kleene
+        self.active_automata_tag = 'automata_kleene'
+        # self.cancel = False
+        self.speed = 'normal'
+        self.fig, self.ax = plt.subplots(figsize=(12, 10))
+
+    def reset_graph(self):
+        graph = self.active_graph
+        plt.cla()
+        ig.plot(
+            graph,
+            target=self.ax,
+            layout="circle",
+            vertex_size=0.4,
+            vertex_color=["steelblue" if name == "q0" else "light blue" for name in graph.vs["name"]],
+            vertex_frame_width=[5.0 if name in self.active_automata.final_states else 1.0 for name in graph.vs["name"]],
+            vertex_frame_color="black",
+            vertex_label=graph.vs["name"],
+            vertex_label_size=20.0
+        )
+
+        self.fig.savefig('./img/graph.svg')
+
+    def update_graph(self, last):
+        graph = self.active_graph
+        plt.cla()
+        ig.plot(
+            graph,
+            target=self.ax,
+            layout="circle",
+            vertex_size=0.4,
+            vertex_color=["green" if name == last else "steelblue" if name == 'q0' else "light blue" for name in graph.vs["name"]],
+            vertex_frame_width=[5.0 if name in self.active_automata.final_states else 1.0 for name in graph.vs["name"]],
+            vertex_frame_color="black",
+            vertex_label=graph.vs["name"],
+            vertex_label_size=20.0
+        )
+
+        self.fig.savefig('./img/graph.svg')
+
+    def update_graph_error(self, last):
+        graph = self.active_graph
+        plt.cla()
+        ig.plot(
+            graph,
+            target=self.ax,
+            layout="circle",
+            vertex_size=0.4,
+            vertex_color=["red" if name == last else "steelblue" if name == 'q0' else "light blue" for name in graph.vs["name"]],
+            vertex_frame_width=[5.0 if name in self.active_automata.final_states else 1.0 for name in graph.vs["name"]],
+            vertex_frame_color="black",
+            vertex_label=graph.vs["name"],
+            vertex_label_size=20.0
+        )
+
+        self.fig.savefig('./img/graph.svg')
+
+    def validate_word(self, word):
+        result = self.active_automata.read_input_stepwise(word)
+        return result
+
+
+if __name__ == '__main__':
+    app = AppAutomata()
+
+    for i in app.validate_word('abcd'):
+        print (i)
